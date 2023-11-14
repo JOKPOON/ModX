bba3323d4a250e4dccf2a14523f5f3103ef8ec7c
Peerapat <peerapad.xiv@mail.kmutt.ac.th>
2023-11-15 02:22:43 +0700

    All product Category Done!

--- frontend/src/components/pages/AllProducts/AllProducts.tsx
+++ frontend/src/components/pages/AllProducts/AllProducts.tsx
@@ -7,7 +7,6 @@ interface CategoryButtonProps {
   onClick: () => void;
 }
 
-
 const mockCategories = ["Education", "Clothes", "Electronics", "Accessories"];
 
 const CategoryButton: React.FC<CategoryButtonProps> = ({
@@ -37,6 +36,22 @@ export const AllProducts = () => {
   const [selectedRating, setSelectedRating] = useState<number | null>(null);
   const [minPrice, setMinPrice] = useState<string>("");
   const [maxPrice, setMaxPrice] = useState<string>("");
+  const [showCategories, setShowCategories] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowCategories(window.innerWidth > 468);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const toggleCategories = () => {
+    setShowCategories(!showCategories);
+  };
 
   const handleCategoryButtonClick = (text: string) => {
     setSelectedCategories((prevSelected) =>
@@ -57,7 +72,6 @@ export const AllProducts = () => {
   const handleRatingClick = (rating: number) => {
     setSelectedRating((prevRating) => (prevRating === rating ? null : rating));
   };
-
 
   const RatingButton: React.FC<{ rate: number; onClick: () => void }> = ({
     rate,
@@ -130,48 +144,78 @@ export const AllProducts = () => {
           </div>
         </div>
         <div className="AllProducts__Section">
-          <div className="AllProducts__Categories">
-            <div className="AllProducts__Categories__Text__Container">
-              <div className="AllProducts__Categories__Text__Box">
-                <div className="AllProducts__Categories__Text">Categories</div>
-                {generateCategoryButtons()}
-                <div className="AllProducts__Categories__Text">Price Range</div>
-                <div className="AllProducts__Categories__Price__Range">
-                  <input
-                    type="text"
-                    className="AllProducts__Categories__Price__Range__Input"
-                    placeholder="Min"
-                    value={minPrice}
-                    onChange={handleMinPriceChange}
-                  />
-                  <div className="AllProducts__Categories__Price__Range__line"></div>
-                  <input
-                    type="text"
-                    className="AllProducts__Categories__Price__Range__Input"
-                    placeholder="Max"
-                    value={maxPrice}
-                    onChange={handleMaxPriceChange}
-                  />
-                </div>
-                <div className="AllProducts__Categories__Text">Rating</div>
-                <div className="AllProducts__Categories__Rating__Container">
-                  <RatingButton rate={1} onClick={() => handleRatingClick(1)} />
-                  <RatingButton rate={2} onClick={() => handleRatingClick(2)} />
-                  <RatingButton rate={3} onClick={() => handleRatingClick(3)} />
-                  <RatingButton rate={4} onClick={() => handleRatingClick(4)} />
-                  <RatingButton rate={5} onClick={() => handleRatingClick(5)} />
-                </div>
-                <div className="AllProducts__Categories__Apply">
-                  <button
-                    className="AllProducts__Categories__Apply__Button"
-                    onClick={handleApplyButtonClick}
-                  >
-                    Apply
-                  </button>
+          {showCategories && (
+            <div className="AllProducts__Categories">
+              <div className="AllProducts__Categories__Text__Container">
+                <div className="AllProducts__Categories__Text__Box">
+                  <div className="AllProducts__Categories__Text">
+                    Categories
+                  </div>
+                  {generateCategoryButtons()}
+                  <div className="AllProducts__Categories__Text">
+                    Price Range
+                  </div>
+                  <div className="AllProducts__Categories__Price__Range">
+                    <input
+                      type="text"
+                      className="AllProducts__Categories__Price__Range__Input"
+                      placeholder="Min"
+                      value={minPrice}
+                      onChange={handleMinPriceChange}
+                    />
+                    <div className="AllProducts__Categories__Price__Range__line"></div>
+                    <input
+                      type="text"
+                      className="AllProducts__Categories__Price__Range__Input"
+                      placeholder="Max"
+                      value={maxPrice}
+                      onChange={handleMaxPriceChange}
+                    />
+                  </div>
+                  <div className="AllProducts__Categories__Text">Rating</div>
+                  <div className="AllProducts__Categories__Rating__Container">
+                    <RatingButton
+                      rate={1}
+                      onClick={() => handleRatingClick(1)}
+                    />
+                    <RatingButton
+                      rate={2}
+                      onClick={() => handleRatingClick(2)}
+                    />
+                    <RatingButton
+                      rate={3}
+                      onClick={() => handleRatingClick(3)}
+                    />
+                    <RatingButton
+                      rate={4}
+                      onClick={() => handleRatingClick(4)}
+                    />
+                    <RatingButton
+                      rate={5}
+                      onClick={() => handleRatingClick(5)}
+                    />
+                  </div>
+                  <div className="AllProducts__Categories__Apply">
+                    <button
+                      className="AllProducts__Categories__Apply__Button"
+                      onClick={handleApplyButtonClick}
+                    >
+                      Apply
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
+
+          <div className="Toggle__Categories__Container">
+              <button onClick={toggleCategories}
+              className="Toggle__Categories" >
+                {showCategories ? 
+                <i className="bx bx-chevron-left"></i> :
+                <i className="bx bx-sort"></i>}
+              </button>
+            </div>
           <div className="AllProducts__Products">
             <div className="AllProducts__Products__Container"></div>
           </div>
