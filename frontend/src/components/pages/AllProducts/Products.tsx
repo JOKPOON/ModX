44aa99d88a0b3d61d76761457a30bf9ec3d3c874
Jirapat <bukharney@gmail.com>
2023-11-20 17:17:53 +0700

    trying: Get all products

--- frontend/src/components/pages/AllProducts/Products.tsx
+++ frontend/src/components/pages/AllProducts/Products.tsx
@@ -1,102 +1,114 @@
+import { useEffect, useState } from "react";
 import "./Products.css";
 import { useNavigate } from "react-router-dom";
 
+// interface items {
+//   picture?: string;
+//   name: string;
+//   sold: number;
+//   price: number;
+// }
+
 interface items {
-  picture?: string;
-  name: string;
-  sold: number;
+  title: string;
   price: number;
+  picture: string;
+  sold: number;
 }
 
-const ProductsData: items[] = [
-  {
-    picture:
-      "https://image.makewebeasy.net/makeweb/m_1920x0/o3WoPJcHm/content/%E0%B9%80%E0%B8%97%E0%B8%84%E0%B8%99%E0%B8%B4%E0%B8%84%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A5%E0%B8%B7%E0%B8%AD%E0%B8%81%E0%B8%8B%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B9%80%E0%B8%82%E0%B9%87%E0%B8%A1%E0%B8%82%E0%B8%B1%E0%B8%94%E0%B8%9C%E0%B8%B9%E0%B9%89%E0%B8%8A%E0%B8%B2%E0%B8%A2.jpg",
-    name: "เข็มขัดผู้ชาย สำหรับนักศึกษาชาย",
-    sold: 40000,
-    price: 999,
-  },
-  {
-    picture:
-      "https://down-th.img.susercontent.com/file/91267398e5330558c9bda5cfab7b5fd4",
-    name: "เครื่องคิดเลข CASIO รุ่น MX-120B",
-    sold: 20,
-    price: 960,
-  },
-  {
-    picture:
-      "https://shoppo-file.sgp1.cdn.digitaloceanspaces.com/natpopshop/product-images/310960155_477469997675145_5855571439791689059_n.jpeg",
-    name: "ปากกาเจลวันพีช One-piece Gel Pen M&G 0.5mm Blue ink ( 5 ด้าม/แพ็ค)",
-    sold: 10,
-    price: 99,
-  },
-  {
-    picture:
-      "https://shopee.co.th/blog/wp-content/uploads/2019/10/FACTFULNESS-%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87-%E0%B9%86-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99.png",
-    name: "FACTFULNESS จริง ๆ แล้วโลกดีขึ้นทุกวัน",
-    sold: 9,
-    price: 800,
-  },
-  {
-    picture:
-      "https://shopee.co.th/blog/wp-content/uploads/2019/10/21-%E0%B8%9A%E0%B8%97%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99.png",
-    name: "21 บทเรียน สำหรับศตวรรษที่ 21 : 21 lessons for 21st century",
-    sold: 20,
-    price: 1590,
-  },
-  {
-    picture:
-      "https://image.makewebeasy.net/makeweb/m_1920x0/o3WoPJcHm/content/%E0%B9%80%E0%B8%97%E0%B8%84%E0%B8%99%E0%B8%B4%E0%B8%84%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A5%E0%B8%B7%E0%B8%AD%E0%B8%81%E0%B8%8B%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B9%80%E0%B8%82%E0%B9%87%E0%B8%A1%E0%B8%82%E0%B8%B1%E0%B8%94%E0%B8%9C%E0%B8%B9%E0%B9%89%E0%B8%8A%E0%B8%B2%E0%B8%A2.jpg",
-    name: "เข็มขัดผู้ชาย สำหรับนักศึกษาชาย",
-    sold: 40000,
-    price: 999,
-  },
-  {
-    picture:
-      "https://down-th.img.susercontent.com/file/91267398e5330558c9bda5cfab7b5fd4",
-    name: "เครื่องคิดเลข CASIO รุ่น MX-120B",
-    sold: 20,
-    price: 960,
-  },
-  {
-    picture:
-      "https://shoppo-file.sgp1.cdn.digitaloceanspaces.com/natpopshop/product-images/310960155_477469997675145_5855571439791689059_n.jpeg",
-    name: "ปากกาเจลวันพีช One-piece Gel Pen M&G 0.5mm Blue ink ( 5 ด้าม/แพ็ค)",
-    sold: 10,
-    price: 99,
-  },
-  {
-    picture:
-      "https://shopee.co.th/blog/wp-content/uploads/2019/10/FACTFULNESS-%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87-%E0%B9%86-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99.png",
-    name: "FACTFULNESS จริง ๆ แล้วโลกดีขึ้นทุกวัน",
-    sold: 9,
-    price: 800,
-  },
-  {
-    picture:
-      "https://shopee.co.th/blog/wp-content/uploads/2019/10/21-%E0%B8%9A%E0%B8%97%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99.png",
-    name: "21 บทเรียน สำหรับศตวรรษที่ 21 : 21 lessons for 21st century",
-    sold: 20,
-    price: 1590,
-  },
-  {
-    picture:
-      "https://shopee.co.th/blog/wp-content/uploads/2019/10/FACTFULNESS-%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87-%E0%B9%86-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99.png",
-    name: "FACTFULNESS จริง ๆ แล้วโลกดีขึ้นทุกวัน",
-    sold: 9,
-    price: 800,
-  },
-  {
-    picture:
-      "https://shopee.co.th/blog/wp-content/uploads/2019/10/21-%E0%B8%9A%E0%B8%97%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99.png",
-    name: "21 บทเรียน สำหรับศตวรรษที่ 21 : 21 lessons for 21st century",
-    sold: 20,
-    price: 1590,
-  },
-];
+// const ProductsData: items[] = [
+//   {
+//     picture:
+//       "https://image.makewebeasy.net/makeweb/m_1920x0/o3WoPJcHm/content/%E0%B9%80%E0%B8%97%E0%B8%84%E0%B8%99%E0%B8%B4%E0%B8%84%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A5%E0%B8%B7%E0%B8%AD%E0%B8%81%E0%B8%8B%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B9%80%E0%B8%82%E0%B9%87%E0%B8%A1%E0%B8%82%E0%B8%B1%E0%B8%94%E0%B8%9C%E0%B8%B9%E0%B9%89%E0%B8%8A%E0%B8%B2%E0%B8%A2.jpg",
+//     name: "เข็มขัดผู้ชาย สำหรับนักศึกษาชาย",
+//     sold: 40000,
+//     price: 999,
+//   },
+//   {
+//     picture:
+//       "https://down-th.img.susercontent.com/file/91267398e5330558c9bda5cfab7b5fd4",
+//     name: "เครื่องคิดเลข CASIO รุ่น MX-120B",
+//     sold: 20,
+//     price: 960,
+//   },
+//   {
+//     picture:
+//       "https://shoppo-file.sgp1.cdn.digitaloceanspaces.com/natpopshop/product-images/310960155_477469997675145_5855571439791689059_n.jpeg",
+//     name: "ปากกาเจลวันพีช One-piece Gel Pen M&G 0.5mm Blue ink ( 5 ด้าม/แพ็ค)",
+//     sold: 10,
+//     price: 99,
+//   },
+//   {
+//     picture:
+//       "https://shopee.co.th/blog/wp-content/uploads/2019/10/FACTFULNESS-%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87-%E0%B9%86-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99.png",
+//     name: "FACTFULNESS จริง ๆ แล้วโลกดีขึ้นทุกวัน",
+//     sold: 9,
+//     price: 800,
+//   },
+//   {
+//     picture:
+//       "https://shopee.co.th/blog/wp-content/uploads/2019/10/21-%E0%B8%9A%E0%B8%97%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99.png",
+//     name: "21 บทเรียน สำหรับศตวรรษที่ 21 : 21 lessons for 21st century",
+//     sold: 20,
+//     price: 1590,
+//   },
+//   {
+//     picture:
+//       "https://image.makewebeasy.net/makeweb/m_1920x0/o3WoPJcHm/content/%E0%B9%80%E0%B8%97%E0%B8%84%E0%B8%99%E0%B8%B4%E0%B8%84%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A5%E0%B8%B7%E0%B8%AD%E0%B8%81%E0%B8%8B%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B9%80%E0%B8%82%E0%B9%87%E0%B8%A1%E0%B8%82%E0%B8%B1%E0%B8%94%E0%B8%9C%E0%B8%B9%E0%B9%89%E0%B8%8A%E0%B8%B2%E0%B8%A2.jpg",
+//     name: "เข็มขัดผู้ชาย สำหรับนักศึกษาชาย",
+//     sold: 40000,
+//     price: 999,
+//   },
+//   {
+//     picture:
+//       "https://down-th.img.susercontent.com/file/91267398e5330558c9bda5cfab7b5fd4",
+//     name: "เครื่องคิดเลข CASIO รุ่น MX-120B",
+//     sold: 20,
+//     price: 960,
+//   },
+//   {
+//     picture:
+//       "https://shoppo-file.sgp1.cdn.digitaloceanspaces.com/natpopshop/product-images/310960155_477469997675145_5855571439791689059_n.jpeg",
+//     name: "ปากกาเจลวันพีช One-piece Gel Pen M&G 0.5mm Blue ink ( 5 ด้าม/แพ็ค)",
+//     sold: 10,
+//     price: 99,
+//   },
+//   {
+//     picture:
+//       "https://shopee.co.th/blog/wp-content/uploads/2019/10/FACTFULNESS-%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87-%E0%B9%86-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99.png",
+//     name: "FACTFULNESS จริง ๆ แล้วโลกดีขึ้นทุกวัน",
+//     sold: 9,
+//     price: 800,
+//   },
+//   {
+//     picture:
+//       "https://shopee.co.th/blog/wp-content/uploads/2019/10/21-%E0%B8%9A%E0%B8%97%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99.png",
+//     name: "21 บทเรียน สำหรับศตวรรษที่ 21 : 21 lessons for 21st century",
+//     sold: 20,
+//     price: 1590,
+//   },
+//   {
+//     picture:
+//       "https://shopee.co.th/blog/wp-content/uploads/2019/10/FACTFULNESS-%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87-%E0%B9%86-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%82%E0%B8%B6%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%99.png",
+//     name: "FACTFULNESS จริง ๆ แล้วโลกดีขึ้นทุกวัน",
+//     sold: 9,
+//     price: 800,
+//   },
+//   {
+//     picture:
+//       "https://shopee.co.th/blog/wp-content/uploads/2019/10/21-%E0%B8%9A%E0%B8%97%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99.png",
+//     name: "21 บทเรียน สำหรับศตวรรษที่ 21 : 21 lessons for 21st century",
+//     sold: 20,
+//     price: 1590,
+//   },
+// ];
+
+//fetch data from backend
 
 const Products = () => {
+  const [ProductsData, setProducts] = useState<items[]>([]);
   const navigate = useNavigate();
+
   const formatPrice = (price: number) => {
     const priceString = price.toString();
     const formatPriced = priceString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -115,65 +127,100 @@ const Products = () => {
       ProductsData.slice(rowIndex * itemsPerRow, (rowIndex + 1) * itemsPerRow)
   );
 
-  const handleAddToCartClick = (item: items, rowIndex: number, itemIndex: number) => {
+  const handleAddToCartClick = (
+    item: items,
+    rowIndex: number,
+    itemIndex: number
+  ) => {
     const overallIndex = rowIndex * itemsPerRow + itemIndex;
     console.log(`Selected item at index : ${overallIndex}:`, item);
-    navigate("/SingleProduct", { state: { selectedItems: [item] }});
+    navigate("/SingleProduct", { state: { selectedItems: [item] } });
   };
 
-  const handleAddToWishlist = (item: items, rowIndex: number, itemIndex: number) => {
+  const handleAddToWishlist = (
+    item: items,
+    rowIndex: number,
+    itemIndex: number
+  ) => {
     const overallIndex = rowIndex * itemsPerRow + itemIndex;
     console.log(`Add item to wishlist at index : ${overallIndex}:`, item);
   };
 
-  return (
-    <div className="AllProducts__Products__Container">
-      <div className="AllProducts__Products__Rows">
-        {rows.map((row, rowIndex) => (
-          <div className="AllProducts__Products__Items" key={rowIndex}>
-            {row.map((item, itemIndex) => (
-              <div className="AllProducts__Products__Item" key={itemIndex}>
-                <button className="AllProducts__Products__Item__Wishlist"
-                onClick={() => handleAddToWishlist(item, rowIndex, itemIndex)}
-                >
-                  <i className="bx bx-heart"></i>
-                </button>
-                <div
-                  className="AllProducts__Products__Item__Picture"
-                  style={{ backgroundImage: `url(${item.picture})` }}
-                ></div>
+  const fetchProducts = async () => {
+    const res = await fetch("http://localhost:8080/v1/product/all", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data[0].title);
+    setProducts(data);
+  };
 
-                <div className="AllProducts__Products__Item__Text">
-                  <div className="AllProducts__Products__Item__Name">
-                    {item.name}
-                  </div>
-                  <div>
-                    <div className="AllProducts__Products__Item__Buttom">
-                      <div className="AllProducts__Products__Item__Buttom__Text">
-                        <div className="AllProducts__Products__Item__Sold">
-                          Sold: {formatPrice(item.sold)}
-                        </div>
-                        <div className="AllProducts__Products__Item__Price">
-                          Price: {formatPrice(item.price)} THB
-                        </div>
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      {ProductsData.length > 0 ? (
+        <div className="AllProducts__Products__Container">
+          <div className="AllProducts__Products__Rows">
+            {rows.map((row, rowIndex) => (
+              <div className="AllProducts__Products__Items" key={rowIndex}>
+                {row.map((item, itemIndex) => (
+                  <div className="AllProducts__Products__Item" key={itemIndex}>
+                    <button
+                      className="AllProducts__Products__Item__Wishlist"
+                      onClick={() =>
+                        handleAddToWishlist(item, rowIndex, itemIndex)
+                      }
+                    >
+                      <i className="bx bx-heart"></i>
+                    </button>
+                    <div
+                      className="AllProducts__Products__Item__Picture"
+                      style={{ backgroundImage: `url(${item.picture})` }}
+                    ></div>
+
+                    <div className="AllProducts__Products__Item__Text">
+                      <div className="AllProducts__Products__Item__Name">
+                        {item.title}
                       </div>
-                      <div className="AllProducts__Products__Item__Button">
-                        <button
-                          className="AllProducts__Products__Item__Button__Add"
-                          onClick={() => handleAddToCartClick(item, rowIndex, itemIndex)}
-                        >
-                          <i className="bx bx-right-arrow-alt"></i>
-                        </button>
+                      <div>
+                        <div className="AllProducts__Products__Item__Buttom">
+                          <div className="AllProducts__Products__Item__Buttom__Text">
+                            <div className="AllProducts__Products__Item__Sold">
+                              Sold: {formatPrice(item.sold)}
+                            </div>
+                            <div className="AllProducts__Products__Item__Price">
+                              Price: {formatPrice(item.price)} THB
+                            </div>
+                          </div>
+                          <div className="AllProducts__Products__Item__Button">
+                            <button
+                              className="AllProducts__Products__Item__Button__Add"
+                              onClick={() =>
+                                handleAddToCartClick(item, rowIndex, itemIndex)
+                              }
+                            >
+                              <i className="bx bx-right-arrow-alt"></i>
+                            </button>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <>Loading</>
+      )}
+    </>
   );
 };
 
