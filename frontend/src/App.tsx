6aaa23e6316c029064c9c897851d2f60331ad869
BigZ <pongsakornjansanit@hotmail.com>
2023-11-23 02:02:56 +0700

    Account Page Input No constraint No map data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, AllProducts, SingleProduct, Cart,Login } from "./components/pages";
+import { Home, AllProducts, SingleProduct, Cart,Login ,Account } from "./components/pages";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
           <Route path="/AllProducts" element={<AllProducts />} />
           <Route path="/SingleProduct" element={<SingleProduct />} />
           <Route path="/Cart" element={<Cart />} />
-          <Route path="/Account" element={<Login />} />
+          <Route path="/Account" element={<Account />} />
         </Routes>
       </Router>
     </div>
