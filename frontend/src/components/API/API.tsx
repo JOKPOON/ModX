0f2374e558d4689cc5e2265a54fd694b9ff42e09
Jirapat <bukharney@gmail.com>
2023-12-06 00:43:34 +0700

    testing: Config nevigate

--- frontend/src/components/API/API.tsx
+++ frontend/src/components/API/API.tsx
@@ -8,7 +8,7 @@ import {
   cartItems,
 } from "../Interface/Interface";
 
-const URL = "http://localhost:8080/";
+const URL = "https://modx-production.up.railway.app/";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
